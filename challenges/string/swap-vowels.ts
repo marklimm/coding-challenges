1e8e5e01db34ca06d28d89484188ed90622787f1
Mark <mark.limm@gmail.com>
2021-05-08 17:52:40 -0400

    add prettier to eslint rules

--- challenges/string/swap-vowels.ts
+++ challenges/string/swap-vowels.ts
@@ -1,11 +1,7 @@
-
-
 //  challenge question comes from https://buttondown.email/cassidoo/archive/to-make-others-less-happy-is-a-crime-to-make
 //  Write a function that takes in a string and reverses all the vowels in the string.
 
-
 const vowels = ['a', 'e', 'i', 'o', 'u']
-
 
 const isVowel = (letter = '') => {
   // console.log('letter', letter)
@@ -15,20 +11,19 @@ const isVowel = (letter = '') => {
 
 /**
  * a recursive function that takes in a string and reverses all the vowels within the string.
- * @param theString 
- * @returns 
+ * @param theString
+ * @returns
  */
-const reverseVowels = (theString = '')  => {
-
+const reverseVowels = (theString = '') => {
   // console.log('reverseVowels() : theString', theString)
 
   //  return a single character or empty string if that's what was passed in (even if its a vowel there's nothing to reverse it with)
-  if (theString.length <= 1) { return theString}
+  if (theString.length <= 1) {
+    return theString
+  }
 
-  
   const firstChar = theString[0]
   const lastChar = theString[theString.length - 1]
-
 
   if (!isVowel(firstChar)) {
     //  the first character is not a vowel, so don't swap it
@@ -37,29 +32,30 @@ const reverseVowels = (theString = '')  => {
     // console.log('everythingAfterFirstChar, ', everythingAfterFirstChar)
     // return
   }
-  
+
   if (!isVowel(lastChar)) {
     //  the last character is not a vowel, so don't swap it
     const everythingBeforeLastChar = theString.slice(0, theString.length - 1)
     return reverseVowels(everythingBeforeLastChar) + lastChar
     // console.log('everythingBeforeLastChar', everythingBeforeLastChar)
-    // return 
-  } 
+    // return
+  }
 
-    //  both the first and last character are both vowels
-    //  do the swap
+  //  both the first and last character are both vowels
+  //  do the swap
 
-    const everythingInBetween = theString.slice(1, theString.length - 1)
+  const everythingInBetween = theString.slice(1, theString.length - 1)
 
-    return lastChar + reverseVowels(everythingInBetween) + firstChar
-  
+  return lastChar + reverseVowels(everythingInBetween) + firstChar
 }
 
-
-export const testSwapVowels = () : void => {
-
-  console.log('reverseVowels(\'I love burritos\')', reverseVowels('I love burritos'))
-  console.log('reverseVowels(\'spicy food is the best!\')', reverseVowels('spicy food is the best!'))
+export const testSwapVowels = (): void => {
+  console.log(
+    "reverseVowels('I love burritos')",
+    reverseVowels('I love burritos')
+  )
+  console.log(
+    "reverseVowels('spicy food is the best!')",
+    reverseVowels('spicy food is the best!')
+  )
 }
-
-
--- script.ts
+++ script.ts
@@ -1,7 +1,3 @@
 import { testSwapVowels } from './challenges/string/swap-vowels'
 
-console.log('hello node-ts!')
-
-
 testSwapVowels()
-
